f241f71cb6f83326e4d65894cfaaf027c6beb28d
Zach Volchak <zakhar.volchak@hpe.com>
2016-05-09 16:25:06 -0600

    finishing refactor of unittests/

--- unittests/test_all.sh
+++ unittests/test_all.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-./test_package_0ad_data.py -q
-./test_package_album.py -q
-./test_task.py -q
-./test_tmcmd_module.py -q
+TEST_FILES=`find . -name "test_*.py"`
+
+#TEST_FILES=customize_node/*
+for test_file in $TEST_FILES
+do
+    echo "Running -> $PWD ---- $test_file"
+    `python3 $test_file -q`
+done
+
+#./test_package_0ad_data.py -q
+#./test_package_album.py -q
+#./test_task.py -q
+#./test_tmcmd_module.py -q
