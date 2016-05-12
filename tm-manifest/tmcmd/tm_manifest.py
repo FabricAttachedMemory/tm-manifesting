f421c9bf6fdeba967be3f0f2357e6a49a9930991
Zach Volchak <zakhar.volchak@hpe.com>
2016-05-12 15:32:13 -0600

    in progress

---
+++ tm-manifest/tmcmd/tm_manifest.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3 -tt
+from pdb import set_trace
+import os
+from . import tm_base
+
+class TmManifest(tm_base.TmCmd):
+
+    def __init__(self):
+        """
+            Define 'args' for this class.
+        """
+        super().__init__()
+        self.args = {
+            'listnodes' : self.listall,
+            'getnode' : self.show,
+            'setnode' : self.set_node
+        }
+
+
+    def listall(self, arg_list=None, **options):
+        """
+    SYNOPSIS
+        listnodes
+
+    DESCRIPTION
+        List all the available nodes in the network.
+        """
+        super().listall(arg_list, **options)
+        url = "%s%s" % (self.url, 'node/')
+        data = self.http_request(url)
+        return self.to_json(data)
+
+
+    def show(self, target, **options):
+        """
+    SYNOPSIS
+        getnode <name>
+
+    DESCRIPTION
+         Show the manifest name that the specified node is currently directed to
+        use at next boot.
+        """
+        super().show(target, **options)
+        api_url = "%s%s%s" % (self.url, 'node/', self.show_name)
+        data = self.http_request(api_url)
+        return self.to_json(data)
+
+
+    def set_node(self, target, **options):
+        """
+    SYNOPSIS
+        setnode <node name> <manifest.json>
+
+    DESCRIPTION
+            Select the manifest for the specified node and construct a kernel
+        and root FS that the node will use the next time it boots.
+        """
+        assert len(target) >= 2, 'Missing argument: setnode <manifest.json> <node coordinate>!'
+        payload = '{ "manifest" :  "%s" }' % target[0]
+        api_url = '%s/%s/%s' % (self.url, 'node/', target[1])
+        clean_url = os.path.normpath(api_url.split('http://')[1])
+        api_url = 'http://' + clean_url
+        data = self.http_request(api_url, payload=payload)
+        return self.to_json(data)
