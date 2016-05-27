a025072da61d1dca61ad2e711df295171e50fd6f
Zach Volchak <zakhar.volchak@hpe.com>
2016-05-27 10:35:42 -0600

    in progress

--- generate_golden_img.py
+++ generate_golden_img.py
@@ -14,7 +14,10 @@ def main(args):
         TODO: Docstr.
     """
     if os.getuid() != 0:
-        raise EnvironmentError('This script requires root permissions! (forgot sudo?)')
+        raise RuntimeError('This script requires root permissions! (forgot sudo?)')
+
+    if not os.path.isdir('/var/lib/tm-manifesting/sys-images/golden/'):
+        raise RuntimeError('"tm-manifesting/sys-images/golden/" does not exist!')
 
     cmd = "python ./configs/vmdebootstrap \
             --owner=$LOGNAME --no-default-configs \
@@ -34,7 +37,7 @@ if __name__ == '__main__':
     PARSER.add_argument('-i', '--img-cfg',
                         help='A config file for your golden filesystem image that\
                         will be taken by vmdebootstrap.',
-                        default='/opt/hpetm/manifesting/img.cfg/golden.vmd')
+                        default='configs/filesystem/golden.arm.vmd')
 
     PARSER.add_argument('--verbose',
                         help='Make it talk.',
