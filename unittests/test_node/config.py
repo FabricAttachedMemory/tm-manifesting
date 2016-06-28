a929e955992272f58c128c983a6782d77ecbe286
Zach Volchak <zakhar.volchak@hpe.com>
2016-06-28 17:04:07 -0600

    in progress

--- unittests/test_node/config.py
+++ unittests/test_node/config.py
@@ -1,3 +1,9 @@
 #/usr/bin/python3 -tt
+import os
+
+tmconfig_path = os.path.realpath(__file__)
+tmconfig_path = os.path.dirname(tmconfig_path)
+TMCONFIG = '%s/hpetmconfig.json' % tmconfig_path
+
 
 MANIFESTING_SERVER = 'http://zachv.americas.hpqcorp.net:31178/manifesting/api/'
--- unittests/test_node/test_node_unbind.py
+++ unittests/test_node/test_node_unbind.py
@@ -7,17 +7,25 @@ import unittest
 import tmcmd
 
 import config
+from tm_librarian.tmconfig import TMConfig
 from pdb import set_trace
 
 class UnbindNodeTest(unittest.TestCase):
 
     tm_cmd = None
+    tmconfig = None
+    node = None
+    manifest_for_test = None
 
     @classmethod
     def setUp(cls):
         """
         """
+        set_trace()
         cls.tm_cmd = tmcmd.tmnode
+        cls.tmconfig = TMConfig(config.TMCONFIG)
+        cls.node = cls.tmconfig.nodes[1].coordinate
+        manifest_for_test = os.path.dirname(__file__) + '/manifest.test'
         return None
 
 
@@ -36,13 +44,18 @@ class UnbindNodeTest(unittest.TestCase):
         self.assertTrue(expected_code in output, '404 status code was not returned.')
 
 
-    def test_node_found(self):
+    def test_node_delete(self):
         """
             Test the return code of the existed node.
         First: upload a Test manifest to a server.
         Second: assign a Manifest to a Node
         Third: check if binding is correct
         """
+        expected_code = 204
+
+        output = self.tm_cmd.set_node([self.node, ----------------------])
+
+        output = self.tm_cmd.delete(self.node])
         self.assertTrue(False, 'Not Implemented')
 
 
