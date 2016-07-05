f0f8d940d5c146762814cab92989f1e553707aef
Zach Volchak <zakhar.volchak@hpe.com>
2016-07-05 14:15:27 -0600

    skeleton for environment validation utils

---
+++ utils/climate.py
@@ -0,0 +1,12 @@
+#!/urs/bin/python3 -tt
+
+
+def ratify(path):
+    """
+        Validate path (or list of path) exist on the system. Save each non-existing
+    path and return it in a list in the end.
+
+    :param 'path': [list or str] list of pathes to validate or a signle path.
+    :return: [list] of non-existing pathes.
+    """
+    raise NotImplemented('Developing...')
