2d1e65b366c34ca2dcf3f93135f54c4de5b8e0b4
Rocky Craig <rocky.craig@hpe.com>
2016-07-22 13:00:27 -0600

    Robusticate symlink recovery; clean up output; a little PEP8

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3 -tt
+
 import argparse
-import os
+import errno
 import json
+import os
+import os.path
 import shlex
 import shutil
 import subprocess
@@ -11,35 +14,49 @@ from pdb import set_trace
 
 from utils import utils
 
-build_config = None
+from configs import build_config
+
+S_IFLNK = os.path.stat.S_IFLNK
 
 
 def set_python_lib():
     """
         Create a symlink to manifesting source code to:
     /usr/local/lib/python3.4/dist-packages/tmms so that user could import
-    manifeseting libraries as follows: import tmms.unittests
+    manifesting libraries as follows: import tmms.unittests.
+    setup.py must be at top of manifesting repo tree.
     """
     py_ver = 'python%d.%d' % (sys.version_info.major, sys.version_info.minor)
     paths_to_lib = ('/usr/local/lib/%s/dist-packages' % py_ver,
                     '/%s/lib/%s/dist-packages' % (sys.prefix, py_ver))
 
     manifesting_path = os.path.realpath(__file__)
-    manifesting_path = os.path.dirname(manifesting_path)    # setup script must be in top of tree
+    manifesting_path = os.path.dirname(manifesting_path)
     for path in paths_to_lib:
-        if path in sys.path:
-            path = path + '/tmms'
-            print(' - Creating a symlink from [%s] to [%s]' % (manifesting_path, path))
-            os.symlink(manifesting_path, path)
+        if path not in sys.path:
             break
-    else:
-        raise RuntimeError('Can\'t find suitable path in python environment to link tmms!')
 
+        path = path + '/tmms'
+        print(' - symlink [%s] -> [%s]' % (path, manifesting_path))
+        try:
+            os.symlink(manifesting_path, path)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise RuntimeError('symlink() failed: %s' % str(e))
+            if os.lstat(path).st_mode & S_IFLNK != S_IFLNK:
+                raise RuntimeError('Existing "%s" is not a symlink' % path)
+            if os.path.realpath(path) != manifesting_path:
+                raise RuntimeError(
+                    'Existing "%s" symlink does not point to %s' % (
+                        path, manifesting_path))
+    else:
+        raise RuntimeError(
+            'Can\'t find suitable path in python environment to link tmms!')
 
 
 def _create_env(fields, ignore_list=[]):
     """
-        Create folder tree based of the list of fileds passed, that must comply
+        Create folder tree based of the list of fields passed, that must comply
     with config/build_config/ structure. This function dependent on build_config/
     module and its  variables naming convention.
 
@@ -47,7 +64,6 @@ def _create_env(fields, ignore_list=[]):
                     manifesting entities.
     :param 'ignore_list': [list] of fields to ignore, e.g. GOLDEN_IMAGE
     """
-    print(' ---- Creating manifest and tftp environment ---- ')
     for field in fields:
         if field in ignore_list:
             continue
@@ -68,7 +84,8 @@ def create_folder(path):
 
 def install_packages():
     """
-        Install packages required by manifesting service.
+        Install packages required by manifesting service.  It only needs
+        files from tm-librarian, it won't actually get run from here.
     """
     pkg_list = ['apt-utils', 'vmdebootstrap',
                 'python3-flask', 'python3-requests',
@@ -83,41 +100,39 @@ def install_packages():
             if process.returncode != 0:
                 errors.append('[%s] %s' % (pkg, err.decode()))
         if errors:
-            raise RuntimeError(''.join(errors))
-    except subprocess.CalledProcessError:
-        raise RuntimeError('Error occured during the install of packages.')
+            raise RuntimeError('\n'.join(errors))
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError('"%s" failed: %s' % (cmd, str(e)))
 
 
 def main(args):
     """
         Run all the functions needed to setup tm-manifesting environment:
-    - set PYTHONPATH using .pth file that have a path to the manifesting/ source code.
-    - create folders for manifesting server and manifesting tftp based of the config file
-    - run make_grub_cfg.py script to create grub config files in the tftp folder
-    - run generate_golden_image.py script to generate golden image
+    - install dependent packages (this setup script is a workaround)
+    - introduce a tmms package to Python environment via symlink
+    - create folders for manifesting files and DBs
+    - create folders for TFTP server (dnsmasq)
+    - run make_grub_cfg.py script to populate grub config files under TFTP
     """
     assert os.geteuid() == 0, 'This script requires root permissions'
     assert sys.platform == 'linux'
 
+    print(' ---- Installing dependent L4TM packages ---- ')
     install_packages()
 
     config_path = os.path.realpath(args['config'])
 
+    print(' ---- Creating workaround Python package path ---- ')
     set_python_lib()
 
-    from configs import build_config as BC
-    global build_config
-    build_config = BC
+    print(' ---- Creating manifest environment ---- ')
     build_config.make_config(config_path)
-
-    print()
     fields = build_config._manifest_env
     _create_env(fields, ['GOLDEN_IMAGE'])
     golden_img_dir = os.path.dirname(build_config.settings['GOLDEN_IMAGE'])
     create_folder(golden_img_dir)
 
-    print()
-
+    print(' ---- Creating TFTP environment ---- ')
     fields = build_config._tftp_env
     _create_env(fields)
     print()
@@ -135,9 +150,13 @@ if __name__ == '__main__':
                         default='configs/hpetmconfig.py')
 
     args, _ = parser.parse_known_args()
+    errmsg = ''     # establish scope
     try:
         main(vars(args))
+        raise SystemExit(0)
     except RuntimeError as err:
-        raise SystemExit('Setup failed:\n%s' % err)
+        errmsg = str(err)
     except Exception as err:
-        raise SystemExit('Ooops:\n%s' % err)
+        errmsg = 'UNEXPECTED ERROR: %s' % str(err)
+
+    raise SystemExit('%s\nsetup failed' % errmsg)
