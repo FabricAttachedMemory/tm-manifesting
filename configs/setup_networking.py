042d6d2b33f9899e0fdb9ca0dab25fcfc157a829
Rocky Craig <rocky.craig@hpe.com>
2016-08-16 14:08:36 -0600

    Overload setup.py for packaging and manual run after git clone; small networking additions

--- configs/setup_networking.py
+++ configs/setup_networking.py
@@ -236,7 +236,6 @@ class TMgrub(object):
                     raise RuntimeError('TMDOMAIN: cannot resolve "%s"' % FQDN)
                 A = next(iter(answer))
                 self.hostIPs.append(A.address)
-                pass
 
             # Oh yeah...
             try:
@@ -311,6 +310,7 @@ class TMgrub(object):
     def configure_dnsmasq(self):
         # Try to carry on in the face of errors.  dnsmasq may not start but
         # manifest_api should still run.
+        self.morehosts = '# /etc/tmms[PXE_INTERFACE] was invalid and so am I'
         try:
             assert self.pxe_interface in NIF.interfaces(), \
                 'PXE_INTERFACE: no such interface "%s"' % self.pxe_interface
@@ -321,13 +321,16 @@ class TMgrub(object):
                     'PXE_INTERFACE "%s" has multiple IPs assigned to it' % \
                     self.pxe_interface
                 ifaceaddr = ifaceaddr.pop()
-                self.morehosts = '%s torms' % ifaceaddr['addr']
+                self.addr = ifaceaddr['addr']
                 self.broadcast = ifaceaddr['broadcast']
                 self.netmask = ifaceaddr['netmask']
+                # Check the __doc__ string for this class
+                set_trace()
+                self.network = IPNetwork(self.addr + '/' + self.netmask)
+                self.morehosts = '%s torms' % self.addr
         except Exception as e:
             print(str(e))
             ifaceaddr = None
-            self.morehosts = '# /etc/tmms[PXE_INTERFACE] was invalid and so am I'
 
         # Fill in other fields, maybe with SWAG kludges
         self.evaluate_tmdomain(ifaceaddr)
